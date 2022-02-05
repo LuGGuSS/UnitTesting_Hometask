c45e8f90277b06861655c3fa26030d7ea15725ac
Yurii Ponomarenko <Yurii_Ponomarenko@epam.com>
2022-02-05 16:16:59 +0200

    Implemented position of the duplicate test.

--- UnitTesting_Hometask/Tests.cs
+++ UnitTesting_Hometask/Tests.cs
@@ -21,5 +21,17 @@ namespace UnitTesting_Hometask
             Assert.AreEqual(rul.ReadRecord(1), "Second record");
             Assert.AreEqual(rul.ReadRecord(0), "First record");
         }
+
+        [TestMethod]
+        public void DuplicatePositioningTest()
+        {
+            var rul = new RecentlyUsedList();
+
+            rul.AddRecord("First record");
+            rul.AddRecord("Second record");
+            rul.AddRecord("First record");
+
+            Assert.AreEqual(rul.ReadRecord(1), "First record");
+        }
     }
 }
