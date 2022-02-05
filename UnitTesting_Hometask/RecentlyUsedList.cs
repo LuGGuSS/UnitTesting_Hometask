f4c72dd1bb8ec00908639f0b8edf468254d54834
Yurii Ponomarenko <Yurii_Ponomarenko@epam.com>
2022-02-05 16:56:17 +0200

    Implemented Max size test

--- UnitTesting_Hometask/RecentlyUsedList.cs
+++ UnitTesting_Hometask/RecentlyUsedList.cs
@@ -10,8 +10,16 @@ namespace UnitTesting_Hometask
     {
 
         private List<string> _recentlyUsedList = new List<string>();
-        
-        
+
+        public int MaxSize = 0;
+
+        public RecentlyUsedList(){}
+        public RecentlyUsedList(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+
         public void AddRecord(string newRecord)
         {
             if (_recentlyUsedList.Contains(newRecord))
--- UnitTesting_Hometask/Tests.cs
+++ UnitTesting_Hometask/Tests.cs
@@ -10,6 +10,10 @@ namespace UnitTesting_Hometask
     [TestClass]
     public class RecentlyUsedListTests
     {
+        public RecentlyUsedListTests()
+        {
+        }
+
         [TestMethod]
         public void AddingItemTest()
         {
@@ -32,6 +36,19 @@ namespace UnitTesting_Hometask
             rul.AddRecord("First record");
 
             Assert.AreEqual(rul.ReadRecord(0), "First record");
+        }
+
+        [TestMethod]
+        public void MaxSizeTest()
+        {
+            var rul = new RecentlyUsedList(2);
+
+            rul.AddRecord("First record");
+            rul.AddRecord("Second record");
+            rul.AddRecord("Third record");
+
+            Assert.ThrowsException<IndexOutOfRangeException>((() => rul.ReadRecord(2)));
+            Assert.AreEqual(rul.ReadRecord(0), "Third record");
         }
     }
 }
