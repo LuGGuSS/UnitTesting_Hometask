da5d0ca10aae16702bed5094e89eb3a7fb98180e
Yurii_Ponomarenko <Yurii_Ponomarenko@EPUAKYIW186E>
2022-02-05 15:33:00 +0200

    Add project files.

---
+++ UnitTesting_Hometask/RecentlyUsedList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting_Hometask
+{
+    class RecentlyUsedList
+    {
+        public void AddRecord(string secondRecord)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTesting_Hometask/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting_Hometask
+{
+    [TestClass]
+    public class RecentlyUsedListTests
+    {
+        public void AddingItemTest()
+        {
+            var rul = new RecentlyUsedList();
+
+            rul.AddRecord("First record");
+            rul.AddRecord("Second record");
+
+            Assert.AreEqual(rul.ReadRecord(1), "Second record");
+            Assert.AreEqual(rul.ReadRecord(0), "First record");
+        }
+    }
+}
