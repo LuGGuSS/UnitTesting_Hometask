01764b42311c5e1e351a372bc967e1feddcb3940
Yurii Ponomarenko <Yurii_Ponomarenko@epam.com>
2022-02-05 17:58:10 +0200

    Created StringSum solution and implementet first test to  check addition of strings.

---
+++ StringSum/StringSum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSum
+{
+    public class StringSum
+    {
+        public string Sum(string s1, string s2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StringSum/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringSum
+{
+    [TestClass]
+    class Tests
+    {
+        [TestMethod]
+        public void SumTest()
+        {
+            var strSum = new StringSum();
+
+            Assert.AreEqual(strSum.Sum("10","1"), "11");
+        }
+
+    }
+}
