52b497066a98527eeb90de8a46f8a4792d464bf4
Yurii Ponomarenko <Yurii_Ponomarenko@epam.com>
2022-02-06 13:06:03 +0200

    Created WordWrap solution with first test "WrapingTest" and empty WordWrap class

---
+++ WordWrap/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordWrap
+{ 
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void WrappingTest()
+        {
+            var ww = new WordWrap();
+
+            var st = "Wrap this test";
+
+            Assert.AreEqual(ww.Wrap(st,4), "W\nrap t\nhis t\next");
+        }
+    }
+}
